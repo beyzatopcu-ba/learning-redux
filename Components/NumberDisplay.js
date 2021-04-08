917513a01a7d41fdb37c033fd2f918af402840e2
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-08 11:50:58 +0300

    added ui

---
+++ Components/NumberDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const NumberDisplay = props => {
+    return (
+        <Text style={{fontSize: 100}}>0</Text>
+    );
+};
+
+export default NumberDisplay;
